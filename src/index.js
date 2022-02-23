1c5c14434fa53acdc1a8469511dadcf4f1b545fc
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-02-23 17:38:55 -0500

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const fetch = require("node-fetch");
 var TronWeb = require('tronweb');
+const BigNumber = require('bignumber.js');
 
 require('dotenv').config();
 var cors = require('cors');
@@ -12,9 +13,10 @@ const app = express();
 app.use(cors());
 
 const port = process.env.PORT || "3003";
-const token = process.env.APP_MT;
+const TOKEN = process.env.APP_MT;
 const uri = process.env.APP_URI;
 const TRONGRID_API = process.env.APP_API || "https://api.trongrid.io";
+const TRONGRID_API_EVENT = process.env.APP_API_EVENT || "https://api.trongrid.io";
 
 const contractAddress = process.env.APP_CONTRACT || "TF1aXPN5kZwPsaFjrFPD7jBKPpAzXYdR8S";
 
@@ -23,7 +25,7 @@ console.log(TRONGRID_API);
 TronWeb = new TronWeb(
   TRONGRID_API,
   TRONGRID_API,
-  TRONGRID_API
+  TRONGRID_API_EVENT
 );
 
 TronWeb.setAddress('TEf72oNbP7AxDHgmb2iFrxE2t1NJaLjTv5');
@@ -70,29 +72,44 @@ app.get('/consultar/transaccion/:id', async(req,res) => {
 
     let id = req.params.id;
 
-    TronWeb.trx.getTransaction(id)
-    .then(value=>{
+    var evento = await TronWeb.getEventByTransactionID(id)
+    evento = evento[0];
+
+    console.log(evento)
+
+    var value = await TronWeb.trx.getTransaction(id)
+
+      //console.log(value)
     //  console.log(value.ret[0].contractRet);
 
-      if (value.ret[0].contractRet === 'SUCCESS') {
+      if (value.ret[0].contractRet === 'SUCCESS' && evento.name === 'Transfer' ) {
 
-        res.send({result: true});
+        value = new BigNumber(evento.result.value);
+        value = value.shiftedBy(-6).toNumber();
+
+        res.send({
+          result: true,
+          value: value,
+          from: TronWeb.address.fromHex(evento.result.from),
+          to: TronWeb.address.fromHex(evento.result.to)
+        });
       }else {
-        res.send({result: false});
+        res.send({
+          result: false,
+          value: 0
+        });
       }
-    })
-    .catch(value=>{
-      console.log(value);
-      res.send({result: false});
-    })
+    
 
 });
 
 
 app.post('/enviar/usdt', async(req,res) => {
-  var wallet =  req.body.wallet
+  var wallet =  req.body.wallet;
+  var cantidad = req.body.cantidad;
+  var token = req.body.token;
 
-  if (req.body.wallet) {
+  if (token === TOKEN && req.body.wallet && req.body.cantidad) {
     res.send({result:true})
   }else{
     res.send({result:false})
@@ -101,3 +118,4 @@ app.post('/enviar/usdt', async(req,res) => {
 });
 
 app.listen(port, ()=> console.log('Escuchando Puerto: ' + port))
+
