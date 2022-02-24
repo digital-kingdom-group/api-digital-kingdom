7ba5c9c5142b001590d744b744f0e75342a7caa5
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2022-02-24 08:36:51 -0500

    update

--- src/index.js
+++ src/index.js
@@ -14,21 +14,20 @@ app.use(cors());
 
 const port = process.env.PORT || "3003";
 const TOKEN = process.env.APP_MT;
-const uri = process.env.APP_URI;
+const PryKey = process.env.APP_PRYKEY;
 const TRONGRID_API = process.env.APP_API || "https://api.trongrid.io";
 const TRONGRID_API_EVENT = process.env.APP_API_EVENT || "https://api.trongrid.io";
 
-const contractAddress = process.env.APP_CONTRACT || "TF1aXPN5kZwPsaFjrFPD7jBKPpAzXYdR8S";
+const contractAddress = process.env.APP_CONTRACT || "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t";
 
-console.log(TRONGRID_API);
-
-TronWeb = new TronWeb(
+var tronWeb = new TronWeb(
   TRONGRID_API,
   TRONGRID_API,
-  TRONGRID_API_EVENT
+  TRONGRID_API_EVENT,
+  PryKey
 );
 
-TronWeb.setAddress('TEf72oNbP7AxDHgmb2iFrxE2t1NJaLjTv5');
+//tronWeb.setAddress('TEf72oNbP7AxDHgmb2iFrxE2t1NJaLjTv5');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -72,12 +71,12 @@ app.get('/consultar/transaccion/:id', async(req,res) => {
 
     let id = req.params.id;
 
-    var evento = await TronWeb.getEventByTransactionID(id)
+    var evento = await tronWeb.getEventByTransactionID(id)
     evento = evento[0];
 
     console.log(evento)
 
-    var value = await TronWeb.trx.getTransaction(id)
+    var value = await tronWeb.trx.getTransaction(id)
 
       //console.log(value)
     //  console.log(value.ret[0].contractRet);
@@ -90,8 +89,8 @@ app.get('/consultar/transaccion/:id', async(req,res) => {
         res.send({
           result: true,
           value: value,
-          from: TronWeb.address.fromHex(evento.result.from),
-          to: TronWeb.address.fromHex(evento.result.to)
+          from: tronWeb.address.fromHex(evento.result.from),
+          to: tronWeb.address.fromHex(evento.result.to)
         });
       }else {
         res.send({
@@ -104,13 +103,42 @@ app.get('/consultar/transaccion/:id', async(req,res) => {
 });
 
 
-app.post('/enviar/usdt', async(req,res) => {
-  var wallet =  req.body.wallet;
-  var cantidad = req.body.cantidad;
-  var token = req.body.token;
+async function enviarUSDT(wallet,cantidadUSDT){
 
-  if (token === TOKEN && req.body.wallet && req.body.cantidad) {
-    res.send({result:true})
+  var contractUSDT = await tronWeb.contract().at("TMZEUaFiGkLYjS7QJ1zKKKNu5hSu6Lno6t"); // NILE token prueba
+
+  cantidadUSDT = parseInt(cantidadUSDT * 10**6);
+
+  if(cantidadUSDT < 1)return "";
+
+  var hash = await contractUSDT.transfer(wallet, cantidadUSDT).send();
+  
+  return hash;
+
+}
+
+
+app.post('/enviar/usdt', async(req,res) => {
+  var envios = JSON.parse(req.body.envios);
+  var token = req.body.token;
+  console.log(envios)
+  if (token === TOKEN && req.body.envios && envios.length) {
+    var respuesta = {result:true};
+    respuesta.data = [];
+    for (let index = 0; index < envios.length; index++) {
+      hash = await enviarUSDT(envios[index].wallet,envios[index].cantidad);
+      if (hash === "") {
+        ok = false;
+      }else{
+        ok = true;
+      }
+      
+      respuesta.data.push({ok:ok ,wallet: envios[index].wallet, cantidad: envios[index].cantidad, hash: hash})
+    }
+
+
+    
+    res.send(respuesta)
   }else{
     res.send({result:false})
   }
